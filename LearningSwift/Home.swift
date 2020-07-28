e57adc784b4e85e3c645e0d832d135645e6ddaf6
Juliette Rapala <juliette@eventric.com>
2020-07-28 16:59:42 -0500

    Add Home and HomeView

---
+++ LearningSwift/Home.swift
@@ -0,0 +1,77 @@
+//
+//  Home.swift
+//  LearningSwiift
+//
+//  Created by Juliette Rapala on 7/24/20.
+//  Copyright © 2020 Juliette Rapala. All rights reserved.
+//
+
+import SwiftUI
+
+struct Home: View {
+    @State var showProfile = false
+    @State var viewState = CGSize.zero
+
+    var body: some View {
+        ZStack {
+            Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1))
+                // Don't add this to a parent. Add this on a per view basis. Not every view can ignore the safe area.
+                .edgesIgnoringSafeArea(.all)
+
+             HomeView(showProfile: $showProfile)
+                // 44 is the size of the top status bar. This will need to be adjusted for non-notched screens.
+                .padding(.top, 44)
+                .background(Color.white)
+                .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+                .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
+                .offset(y: showProfile ? -450 : 0)
+                .rotation3DEffect(Angle(degrees: showProfile ? Double(viewState.height / 10) - 10 : 0), axis: (x:10, y:0, z:0))
+                .scaleEffect(showProfile ? 0.9 : 1)
+                .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+                .edgesIgnoringSafeArea(.all)
+            
+            MenuView()
+                // not visible, but interactive
+                .background(Color.black.opacity(0.001))
+                .offset(y: showProfile ? 0 : screen.height)
+                .offset(y: viewState.height)
+                .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+                .onTapGesture {
+                    self.showProfile.toggle()
+                }
+                .gesture(
+                    DragGesture().onChanged { value in
+                        self.viewState = value.translation
+                    }
+                    .onEnded { value in
+                        if self.viewState.height > 50 {
+                            self.showProfile = false
+                        }
+                        self.viewState = .zero
+                    }
+                )
+        }
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
+
+struct AvatarView: View {
+    @Binding var showProfile: Bool
+
+    var body: some View {
+        Button(action: { self.showProfile.toggle() }) {
+            Image("Avatar")
+                .renderingMode(.original)
+                .resizable()
+                .frame(width: 36, height: 36)
+                .clipShape(Circle())
+        }
+    }
+}
+
+let screen = UIScreen.main.bounds
---
+++ LearningSwift/HomeView.swift
@@ -0,0 +1,98 @@
+//
+//  HomeView.swift
+//  LearningSwiift
+//
+//  Created by Juliette Rapala on 7/26/20.
+//  Copyright © 2020 Juliette Rapala. All rights reserved.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @Binding var showProfile: Bool
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Watching")
+                    .font(.system(size: 28, weight: .bold))
+                
+                Spacer()
+
+                AvatarView(showProfile: $showProfile)
+            }
+            .padding(.horizontal)
+            .padding(.leading, 14)
+            .padding(.top, 30)
+
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 30) {
+                    ForEach(sectionData) { item in
+                        SectionView(section: item)
+                    }
+                }
+                .padding(30)
+                .padding(.bottom, 30)
+            }
+            
+            Spacer()
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView(showProfile: .constant(false))
+    }
+}
+
+struct SectionView: View {
+    var section: Section
+
+    var body: some View {
+        VStack {
+            HStack(alignment: .top) {
+                Text(section.title)
+                    .font(.system(size: 24, weight: .bold))
+                    .frame(width: 160, alignment: .leading)
+                    .foregroundColor(.white)
+                Spacer()
+                Image(section.logo)
+            }
+            
+            Text(section.text.uppercased())
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            section.image
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 210)
+        }
+        .padding(.top, 20)
+        .padding(.horizontal, 20)
+        .frame(width: 275, height: 275)
+        .background(section.color)
+        .cornerRadius(30)
+        .shadow(color: section.color.opacity(0.3), radius: 20, x: 0, y: 20)
+    }
+}
+
+struct Section: Identifiable {
+    var id = UUID()
+    var title: String
+    var text: String
+    var logo: String
+    var image: Image
+    var color: Color
+}
+
+let sectionData = [
+    Section(title: "Prototype Designs in SwiftUI", text: "18 Sections", logo: "Logo1", image: Image("Card1"), color: Color("card1")),
+    
+    Section(title: "Build a SwiftUI App", text: "20 Sections", logo: "Logo1", image: Image(uiImage: #imageLiteral(resourceName: "Background")), color: Color("card2")),
+    
+    Section(title: "SwiftUI Advanced", text: "20 Sections", logo: "Logo1", image: Image(uiImage: #imageLiteral(resourceName: "Card6")), color: Color("card3"))
+]
+
+
+
